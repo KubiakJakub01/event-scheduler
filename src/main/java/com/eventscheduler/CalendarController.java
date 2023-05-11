dc4f3c2d53edc2f6588509eabb142fdef866b9f2
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-05-11 20:52:42 +0200

    Init calendar controller class

---
+++ src/main/java/com/eventscheduler/CalendarController.java
@@ -0,0 +1,37 @@
+package com.eventscheduler;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.time.ZonedDateTime;
+import java.util.*;
+
+public class CalendarController implements Initializable {
+
+    ZonedDateTime dateFocus;
+    ZonedDateTime today;
+
+    @FXML
+    private Text year;
+
+    @FXML
+    private Text month;
+
+    @FXML
+    private FlowPane calendar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        dateFocus = ZonedDateTime.now();
+        today = ZonedDateTime.now();
+    }
+
+
+}
