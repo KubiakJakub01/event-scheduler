a476133b32d2df1d2eab687d7ee196a42215be70
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-05-11 20:51:50 +0200

    Add getters and setters

--- src/main/java/com/eventscheduler/CalendarActivity.java
+++ src/main/java/com/eventscheduler/CalendarActivity.java
@@ -13,6 +13,30 @@ public class CalendarActivity {
         this.serviceNo = serviceNo;
     }
 
+    public ZonedDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(ZonedDateTime date) {
+        this.date = date;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public Integer getServiceNo() {
+        return serviceNo;
+    }
+
+    public void setServiceNo(Integer serviceNo) {
+        this.serviceNo = serviceNo;
+    }
+
     @Override
     public String toString() {
         return "CalenderActivity{" +
