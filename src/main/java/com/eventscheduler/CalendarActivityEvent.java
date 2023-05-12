bec79d4eee9f788ba2e994ec78cc6d2c431bece5
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-05-12 21:27:28 +0200

    Implement listener methods

--- src/main/java/com/eventscheduler/CalendarActivityEvent.java
+++ src/main/java/com/eventscheduler/CalendarActivityEvent.java
@@ -1,4 +1,18 @@
 package com.eventscheduler;
 
-public class CalendarActivityEvent {
+public class CalendarActivityEvent implements CalendarActivityListener{
+    @Override
+    public void addObserver(CalendarActivityListener observer) {
+
+    }
+
+    @Override
+    public void removeObserver(CalendarActivityListener observer) {
+
+    }
+
+    @Override
+    public void notifyObservers(CalendarActivity activity) {
+
+    }
 }
