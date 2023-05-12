189d5b69da440049d3e8d68f4393deaff0d83353
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-05-12 14:52:19 +0200

    Update CalendarActivity fields

--- src/main/java/com/eventscheduler/CalendarActivity.java
+++ src/main/java/com/eventscheduler/CalendarActivity.java
@@ -3,14 +3,28 @@ package com.eventscheduler;
 import java.time.ZonedDateTime;
 
 public class CalendarActivity {
-    private ZonedDateTime date;
-    private String clientName;
-    private Integer serviceNo;
+    private static final System.Logger logger = System.getLogger(CalendarActivity.class.getName());
 
-    public CalendarActivity(ZonedDateTime date, String clientName, Integer serviceNo) {
+    private String title;
+    private ZonedDateTime date;
+    private String place;
+    private Float time;
+    private String description;
+
+    public CalendarActivity(String title, ZonedDateTime date, String clientName, Float time, String description) {
+        this.title = title;
         this.date = date;
-        this.clientName = clientName;
-        this.serviceNo = serviceNo;
+        this.place = clientName;
+        this.time = time;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public ZonedDateTime getDate() {
@@ -21,28 +35,45 @@ public class CalendarActivity {
         this.date = date;
     }
 
-    public String getClientName() {
-        return clientName;
+    public String getPlace() {
+        return place;
     }
 
-    public void setClientName(String clientName) {
-        this.clientName = clientName;
+    public void setPlace(String place) {
+        this.place = place;
     }
 
-    public Integer getServiceNo() {
-        return serviceNo;
+    public Float getTime() {
+        return time;
     }
 
-    public void setServiceNo(Integer serviceNo) {
-        this.serviceNo = serviceNo;
+    public void setTime(Float time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     @Override
     public String toString() {
-        return "CalenderActivity{" +
-                "date=" + date +
-                ", clientName='" + clientName + '\'' +
-                ", serviceNo=" + serviceNo +
-                '}';
+        StringBuffer sb = new StringBuffer();
+        sb.append("CalendarActivity{");
+        sb.append("title='");
+        sb.append(title);
+        sb.append("', date='");
+        sb.append(date);
+        sb.append("', place='");
+        sb.append(place);
+        sb.append("', time='");
+        sb.append(time);
+        sb.append("', description='");
+        sb.append(description);
+        sb.append("'}");
+        return sb.toString();
     }
 }
