8d13b61b16acbb46d6d6d0b24f55d0f2f3ee6681
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-05-12 21:29:45 +0200

    Add implementation to add and remove oberver

--- src/main/java/com/eventscheduler/CalendarActivityEvent.java
+++ src/main/java/com/eventscheduler/CalendarActivityEvent.java
@@ -1,14 +1,19 @@
 package com.eventscheduler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalendarActivityEvent implements CalendarActivityListener{
+    private List<CalendarActivityListener> observers = new ArrayList<>();
+
     @Override
     public void addObserver(CalendarActivityListener observer) {
-
+        observers.add(observer);
     }
 
     @Override
     public void removeObserver(CalendarActivityListener observer) {
-
+        observers.remove(observer);
     }
 
     @Override
