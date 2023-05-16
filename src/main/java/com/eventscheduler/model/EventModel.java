ba953765a3452f43af8ec1ed55609d987cf3d2de
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-05-16 22:17:31 +0200

    Add logger

--- src/main/java/com/eventscheduler/model/EventModel.java
+++ src/main/java/com/eventscheduler/model/EventModel.java
@@ -5,7 +5,10 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.conversions.Bson;
 
+import java.util.List;
+
 public class EventModel {
+    private static final System.Logger logger = System.getLogger(EventModel.class.getName());
 
     private MongoDatabase database;
     private MongoCollection<CalendarActivity> events_collection;
@@ -17,21 +20,25 @@ public class EventModel {
 
     public void addEvent(CalendarActivity event) {
         events_collection.insertOne(event);
+        logger.log(System.Logger.Level.INFO, "Event " + event.getTitle() + " added to database");
     }
 
     public void deleteEvent(CalendarActivity event) {
         events_collection.deleteOne((Bson) event);
+        logger.log(System.Logger.Level.INFO, "Event " + event.getTitle() + " deleted from database");
     }
 
     public void updateEvent(CalendarActivity event) {
         events_collection.updateOne((Bson) event, (Bson) event);
+        logger.log(System.Logger.Level.INFO, "Event " + event.getTitle() + " updated in database");
     }
 
-    public void getEvent(CalendarActivity event) {
-        events_collection.find((Bson) event);
+    public CalendarActivity getEvent(CalendarActivity event) {
+        return (CalendarActivity) events_collection.find((Bson) event);
     }
 
-    public void getAllEvents() {
-        events_collection.find();
+    public List<CalendarActivity> getAllEvents() {
+        logger.log(System.Logger.Level.INFO, "Getting all events from database");
+        return (List<CalendarActivity>) events_collection.find();
     }
 }
