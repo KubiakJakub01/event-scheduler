8ca15208d9fad1445cdb902f81b9b074625b9010
James Kubiak <kubiak.jakub01@gmail.com>
2023-05-17 23:07:16 +0200

    Update db init

--- src/main/java/com/eventscheduler/Main.java
+++ src/main/java/com/eventscheduler/Main.java
@@ -1,17 +1,31 @@
 package com.eventscheduler;
 
+import com.eventscheduler.model.ConnectionDB;
+import com.eventscheduler.model.EventModel;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main extends Application {
+
+    private CalendarController calendarController;
+    private ConnectionDB connectionDB;
+    private EventModel eventModel;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Calendar.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
+        initDBConnection();
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("Calendar.fxml"));
+        Parent root = fxmlLoader.load();
+        calendarController = fxmlLoader.getController();
+        calendarController.loadModel(eventModel);
+        Scene scene = new Scene(root, 1024, 768);
         stage.setTitle("Event scheduler");
         stage.setScene(scene);
         // Set on close request to close all windows
@@ -19,6 +33,11 @@ public class Main extends Application {
             System.exit(0);
         });
         stage.show();
+    }
+
+    public void initDBConnection() {
+        connectionDB = new ConnectionDB();
+        eventModel = new EventModel(connectionDB.getEvents());
     }
 
     public static void main(String[] args) {
