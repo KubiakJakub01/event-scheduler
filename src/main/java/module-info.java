6f02cdd93a8b05de3ddca377552ab7c4cd6982ae
James Kubiak <kubiak.jakub01@gmail.com>
2023-05-17 23:05:47 +0200

    Update module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module com.example.eventscheduler {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.mongodb.driver.sync.client;
+    requires org.mongodb.bson;
+    requires org.mongodb.driver.core;
 
 
     opens com.eventscheduler to javafx.fxml;
     exports com.eventscheduler;
+    exports com.eventscheduler.model;
+    opens com.eventscheduler.model to javafx.fxml;
 }
