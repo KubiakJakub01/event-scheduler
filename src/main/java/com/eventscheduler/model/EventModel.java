9b6dbb7d5d19ff9aeafdbb47a1be4774d935ae58
James Kubiak <kubiak.jakub01@gmail.com>
2023-05-18 23:20:51 +0200

    Add queary to get nearest event

--- src/main/java/com/eventscheduler/model/EventModel.java
+++ src/main/java/com/eventscheduler/model/EventModel.java
@@ -3,12 +3,14 @@ package com.eventscheduler.model;
 import com.eventscheduler.CalendarActivity;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import org.bson.conversions.Bson;
 
-import java.time.ZonedDateTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.mongodb.client.model.Filters.*;
 
 public class EventModel {
     private static final System.Logger logger = System.getLogger(EventModel.class.getName());
@@ -38,6 +40,62 @@ public class EventModel {
         logger.log(System.Logger.Level.INFO, "Getting all events from database");
         List<CalendarActivity> eventsList = events.find().into(new ArrayList<>());
         logger.log(System.Logger.Level.INFO, "Found " + eventsList.size() + " events in database");
+        return eventsList;
+    }
+
+    public List<CalendarActivity> getEventsByMonth(int year, int month) {
+        // Create the start and end date of the month
+        LocalDate startDate = LocalDate.of(year, month, 1);
+        LocalDate endDate = startDate.plusMonths(1).minusDays(1);
+
+        logger.log(System.Logger.Level.INFO, "Getting events from database for month " + month + " of year " + year);
+        // Create the query filter
+        Bson filter = and(
+                eq("$expr", and(
+                        eq("$eq", List.of(
+                                new Document("$year", "$date"),
+                                year
+                        )),
+                        eq("$eq", List.of(
+                                new Document("$month", "$date"),
+                                month
+                        )),
+                        eq("$gte", List.of("$date", startDate.atStartOfDay())),
+                        eq("$lte", List.of("$date", endDate.atTime(23, 59, 59)))
+                ))
+        );
+
+        // Retrieve the events matching the filter
+        List<CalendarActivity> eventsList = new ArrayList<>();
+        try (MongoCursor<CalendarActivity> cursor = events.find(filter).iterator()) {
+            while (cursor.hasNext()) {
+                eventsList.add(cursor.next());
+            }
+        }
+        logger.log(System.Logger.Level.INFO, "Found " + eventsList.size() + " events in database");
+
+        return eventsList;
+    }
+
+    public List<CalendarActivity> getNearestEvents(int limit) {
+        logger.log(System.Logger.Level.INFO, "Getting " + limit + " nearest events from database");
+        // Create the query filter
+        Bson filter = and(
+                eq("$expr", and(
+                        eq("$gte", List.of("$date", LocalDate.now().atStartOfDay())),
+                        eq("$lte", List.of("$date", LocalDate.now().plusDays(7).atTime(23, 59, 59)))
+                ))
+        );
+
+        // Retrieve the events matching the filter
+        List<CalendarActivity> eventsList = new ArrayList<>();
+        try (MongoCursor<CalendarActivity> cursor = events.find(filter).limit(limit).iterator()) {
+            while (cursor.hasNext()) {
+                eventsList.add(cursor.next());
+            }
+        }
+        logger.log(System.Logger.Level.INFO, "Found " + eventsList.size() + " events in database");
+
         return eventsList;
     }
 }
