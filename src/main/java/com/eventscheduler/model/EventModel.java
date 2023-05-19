b5ebd5e19d0fedad194a8071019ecd3a3669d644
James Kubiak <kubiak.jakub01@gmail.com>
2023-05-19 10:54:30 +0200

    Sort given events by date

--- src/main/java/com/eventscheduler/model/EventModel.java
+++ src/main/java/com/eventscheduler/model/EventModel.java
@@ -3,6 +3,7 @@ package com.eventscheduler.model;
 import com.eventscheduler.CalendarActivity;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Sorts;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
@@ -65,9 +66,53 @@ public class EventModel {
                 ))
         );
 
+        // Define the sort order
+        Bson sort = Sorts.ascending("date");
+
         // Retrieve the events matching the filter
         List<CalendarActivity> eventsList = new ArrayList<>();
-        try (MongoCursor<CalendarActivity> cursor = events.find(filter).iterator()) {
+        try (MongoCursor<CalendarActivity> cursor = events.find(filter).sort(sort).iterator()) {
+            while (cursor.hasNext()) {
+                eventsList.add(cursor.next());
+            }
+        }
+        logger.log(System.Logger.Level.INFO, "Found " + eventsList.size() + " events in database");
+
+        return eventsList;
+    }
+
+    public List<CalendarActivity> getEventsByDay(int year, int month, int day){
+        // Create the start and end date of the day
+        LocalDate startDate = LocalDate.of(year, month, day);
+        LocalDate endDate = startDate.plusDays(1);
+
+        logger.log(System.Logger.Level.INFO, "Getting events from database for day " + day + " of month " + month + " of year " + year);
+        // Create the query filter
+        Bson filter = and(
+                eq("$expr", and(
+                        eq("$eq", List.of(
+                                new Document("$year", "$date"),
+                                year
+                        )),
+                        eq("$eq", List.of(
+                                new Document("$month", "$date"),
+                                month
+                        )),
+                        eq("$eq", List.of(
+                                new Document("$dayOfMonth", "$date"),
+                                day
+                        )),
+                        eq("$gte", List.of("$date", startDate.atStartOfDay())),
+                        eq("$lte", List.of("$date", endDate.atTime(23, 59, 59)))
+                ))
+        );
+
+        // Define the sort order
+        Bson sort = Sorts.ascending("date");
+
+        // Retrieve the events matching the filter
+        List<CalendarActivity> eventsList = new ArrayList<>();
+        try (MongoCursor<CalendarActivity> cursor = events.find(filter).sort(sort).iterator()) {
             while (cursor.hasNext()) {
                 eventsList.add(cursor.next());
             }
