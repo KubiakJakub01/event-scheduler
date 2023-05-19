217ab6eff5d3774967607b41aea9d7f842492ad4
KubiakJakub01 <kubiak.jakub01@gmail.com>
2023-05-19 15:21:17 +0200

    Add DBManager

---
+++ src/main/java/com/eventscheduler/model/DBManager.java
@@ -0,0 +1,13 @@
+package com.eventscheduler.model;
+
+import java.util.List;
+
+public interface DBManager<Model> {
+
+    public void addElement(Model model);
+    public void removeElement(Model model);
+    public void updateElement(Model model);
+    public Model getElement(Model model);
+    public List<Model> getAllElements();
+
+}
