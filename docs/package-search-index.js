5174d44de678f975725f79739bcfba451c07fefc
James Kubiak <kubiak.jakub01@gmail.com>
2023-05-21 15:57:42 +0200

    Add code documentation

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.eventscheduler","l":"com.eventscheduler"},{"m":"com.example.eventscheduler","l":"com.eventscheduler.controller"},{"m":"com.example.eventscheduler","l":"com.eventscheduler.model"}];updateSearchResults();
