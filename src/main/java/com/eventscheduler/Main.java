67023049254e685bea00cb4ad5d5fbe64ab93a36
James Kubiak <kubiak.jakub01@gmail.com>
2023-05-21 13:41:11 +0200

    Update start method

--- src/main/java/com/eventscheduler/Main.java
+++ src/main/java/com/eventscheduler/Main.java
@@ -1,6 +1,8 @@
 package com.eventscheduler;
 
 import com.eventscheduler.controller.CalendarController;
+import com.eventscheduler.controller.EventObservable;
+import com.eventscheduler.controller.EventScheduler;
 import com.eventscheduler.model.ConnectionDB;
 import com.eventscheduler.model.EventManager;
 import javafx.application.Application;
@@ -16,6 +18,8 @@ public class Main extends Application {
     private CalendarController calendarController;
     private ConnectionDB connectionDB;
     private EventManager eventManager;
+    private EventObservable eventObservable;
+    private EventScheduler eventScheduler;
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -24,12 +28,13 @@ public class Main extends Application {
         fxmlLoader.setLocation(getClass().getResource("Calendar.fxml"));
         Parent root = fxmlLoader.load();
         calendarController = fxmlLoader.getController();
-        calendarController.loadModel(eventManager);
+        initDependencies();
         Scene scene = new Scene(root, 1024, 768);
         stage.setTitle("Event scheduler");
         stage.setScene(scene);
         // Set on close request to close all windows
         stage.setOnCloseRequest(windowEvent -> {
+            closeDBConnection();
             System.exit(0);
         });
         stage.setResizable(false);
@@ -38,7 +43,20 @@ public class Main extends Application {
 
     public void initDBConnection() {
         connectionDB = new ConnectionDB();
-        eventManager = new EventManager(connectionDB.getEvents());
+        eventManager = new EventManager(connectionDB.getEvents(), eventObservable);
+    }
+
+    public void initDependencies() {
+        eventObservable = new EventObservable();
+        initDBConnection();
+        eventScheduler = new EventScheduler(eventManager);
+        calendarController.initController(eventManager);
+        eventObservable.addObserver(eventScheduler);
+        eventObservable.addObserver(calendarController);
+    }
+
+    public void closeDBConnection() {
+        connectionDB.closeConnection();
     }
 
     public static void main(String[] args) {
