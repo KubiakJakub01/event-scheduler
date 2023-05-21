a53488995f8585e525baa6309d0bf3c906f0e6b6
James Kubiak <kubiak.jakub01@gmail.com>
2023-05-21 14:35:50 +0200

    Add docstring to EventObservable

--- src/main/java/com/eventscheduler/controller/EventObservable.java
+++ src/main/java/com/eventscheduler/controller/EventObservable.java
@@ -1,3 +1,6 @@
+/**
+    EventObservable class for Observer Pattern
+ */
 package com.eventscheduler.controller;
 
 import java.util.ArrayList;
@@ -7,22 +10,41 @@ public class EventObservable implements Observable {
     private static final System.Logger logger = System.getLogger(EventObservable.class.getName());
     private final List<Observer> observers;
 
+    /**
+     * Constructor for EventObservable
+     */
     public EventObservable() {
         observers = new ArrayList<>();
     }
 
+    /**
+     * Adds an observer to the list of observers
+     *
+     * @param observer Observer to be added
+     */
     @Override
     public void addObserver(Observer observer) {
+        logger.log(System.Logger.Level.INFO, "Adding observer " + observer.getClass().getName());
         observers.add(observer);
     }
 
+    /**
+     * Removes an observer from the list of observers
+     *
+     * @param observer Observer to be removed
+     */
     @Override
     public void removeObserver(Observer observer) {
+        logger.log(System.Logger.Level.INFO, "Removing observer " + observer.getClass().getName());
         observers.remove(observer);
     }
 
+    /**
+     * Notifies all observers in the list of observers
+     */
     @Override
     public void notifyObservers() {
+        logger.log(System.Logger.Level.INFO, "Notifying " + observers.size() + " observers");
         for (Observer observer : observers) {
             observer.update();
         }
