1b6869c1378756e897cdacc65b15dcfe23b644f8
James Kubiak <kubiak.jakub01@gmail.com>
2023-05-21 13:40:14 +0200

    Add popup window when event has started

--- src/main/java/com/eventscheduler/controller/EventScheduler.java
+++ src/main/java/com/eventscheduler/controller/EventScheduler.java
@@ -1,7 +1,18 @@
 package com.eventscheduler.controller;
 
+import com.eventscheduler.model.EventManager;
 import com.eventscheduler.model.EventModel;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -9,25 +20,21 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class EventScheduler {
+import static java.lang.Thread.sleep;
+
+public class EventScheduler implements Observer {
     private static final System.Logger logger = System.getLogger(EventScheduler.class.getName());
 
     private ScheduledExecutorService scheduler;
+    private EventManager eventManager;
     private EventModel scheduledEvent;
+    private Stage eventWindow;
 
-    public EventScheduler(EventModel scheduledEvent) {
-        this.scheduledEvent = scheduledEvent;
+    public EventScheduler(EventManager eventManager) {
+        this.eventManager = eventManager;
+        this.scheduledEvent = eventManager.getNearestEvents(1).get(0);
         this.scheduler = Executors.newSingleThreadScheduledExecutor();
         scheduleEvent(scheduledEvent);
-    }
-
-    public void updateScheduledEvent(EventModel scheduledEvent) {
-        if (this.scheduledEvent.equals(scheduledEvent)) {
-            logger.log(System.Logger.Level.INFO, "Event already scheduled");
-            return;
-        }
-        this.scheduledEvent = scheduledEvent;
-        reloadScheduler();
     }
 
     public void scheduleEvent(EventModel event) {
@@ -40,11 +47,40 @@ public class EventScheduler {
 
         scheduler.schedule(() -> {
             // Action to be performed when the event starts
-            logger.log(System.Logger.Level.INFO, "Event scheduled: " + event.getTitle());
+            Platform.runLater(() -> showEventWindow(event));
+            logger.log(System.Logger.Level.INFO, "Event: " + event.getTitle() + " started at " + LocalDateTime.now());
         }, initialDelay, TimeUnit.SECONDS);
     }
 
-    public void shutdownScheduler() {
+    private void showEventWindow(EventModel event) {
+        eventWindow = new Stage();
+        eventWindow.setTitle("Event Started");
+
+        Label titleLabel = new Label("Event: " + event.getTitle());
+        Label dateLabel = new Label("Date: " + event.getDate().toString());
+        Label durationLabel = new Label("Duration: " + event.getDuration().toString());
+        Label placeLabel = new Label("Place: " + event.getPlace());
+        Label descriptionLabel = new Label("Description: " + event.getDescription());
+        Button closeButton = new Button("Close");
+        closeButton.setOnAction(e -> {
+            eventWindow.close();
+            scheduleNextEvent();
+        });
+
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(10));
+        layout.getChildren().addAll(titleLabel, dateLabel, durationLabel, placeLabel, descriptionLabel, closeButton);
+
+        Scene scene = new Scene(layout, 300, 300);
+        eventWindow.setOnCloseRequest(e -> {
+            eventWindow.close();
+            scheduleNextEvent();
+        });
+        eventWindow.setScene(scene);
+        eventWindow.show();
+    }
+
+    private void shutdownScheduler() {
         // Shutdown the scheduler when no longer needed
         scheduler.shutdown();
         logger.log(System.Logger.Level.INFO, "Scheduler shutdown");
@@ -55,5 +91,23 @@ public class EventScheduler {
         shutdownScheduler();
         scheduler = Executors.newSingleThreadScheduledExecutor();
         scheduleEvent(scheduledEvent);
+    }
+
+    private void scheduleNextEvent() {
+        EventModel nextEvent = eventManager.getNearestEvents(1).get(0);
+        this.scheduledEvent = nextEvent;
+        scheduleEvent(nextEvent);
+    }
+
+    @Override
+    public void update() {
+        // Update the scheduler when the event list is updated
+        EventModel newEvent = eventManager.getNearestEvents(1).get(0);
+        if (this.scheduledEvent.equals(newEvent)) {
+            logger.log(System.Logger.Level.INFO, "Event already scheduled");
+            return;
+        }
+        this.scheduledEvent = newEvent;
+        reloadScheduler();
     }
 }
